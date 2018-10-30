4451a5efecf44d92750e4f282c31eac49fd76076
orlandoxtg <orlando.torres@udem.edu>
2018-10-30 16:43:19 -0600

    modifique clase cliente y nombre

--- Cliente.java
+++ Cliente.java
@@ -1,18 +1,18 @@
 
 public class Cliente extends Persona {
-
-    public Cliente(final int ID, Nombre nombre, Direccion direccion, Fecha miembroDesde) {
+   String RFC;
+   
+    public Cliente(int ID, Nombre nombre, Direccion direccion, Fecha miembroDesde, String RFC) {
         super(ID, nombre, direccion, miembroDesde);
     }
 
-    public void setMiembroDesde(Fecha miembroDesde) {
-        this.miembroDesde = miembroDesde;
-    }
-
-    public Fecha getMiembroDesde() {
-        return miembroDesde;
-    }
-
+   public void setRFC(String RFC){
+      this.RFC=RFC;
+   }
+   
+   public String getRFC(){
+      return RFC;
+   }
     public String toString() {
         return "Datos de Cliente: \n" + super.toString();
     }
